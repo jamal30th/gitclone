ceeae73d0e29edadc924d9331befab000d2d0efb
Muhammad Jamal Uddin <jamal30th@gmail.com>
2019-03-06 20:08:05 +0600

    Added another method to Hello java

--- Hello.java
+++ Hello.java
@@ -6,7 +6,11 @@ import java.awt.Color;
 public class Hello {
 public static void main(String[] args) {
     System.out.println("VS Code is awesome");
-    
-}
-    
+    greeting("Muhammad Jamal Uddin");
+} 
+ public static  void  greeting(String name){
+System.out.println("Welcome "+ name);
+
+ 
+ }
 }
